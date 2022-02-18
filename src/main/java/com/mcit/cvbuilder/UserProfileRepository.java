77c86d884c7a13b4b8f108e70c7a824c46ef814e
Phani Devarapu <phanidevarapu321@gmail.com>
2022-02-17 23:00:08 -0500

    Initial commit

---
+++ src/main/java/com/mcit/cvbuilder/UserProfileRepository.java
@@ -0,0 +1,12 @@
+package com.mcit.cvbuilder;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mcit.cvbuilder.models.User;
+import com.mcit.cvbuilder.models.UserProfile;
+
+import java.util.Optional;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
+    Optional<UserProfile> findByUserName(String userName);
+}
